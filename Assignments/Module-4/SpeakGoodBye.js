5dcb5007a59ecc9ddd8b9e0e0540e9e420b1c161
Mohd Inayath Ullah <68437467+Inayath14@users.noreply.github.com>
2020-07-23 21:35:07 +0530

    Add files via upload

---
+++ Assignments/Module-4/SpeakGoodBye.js
@@ -0,0 +1,9 @@
+(function(window) {
+	var speakWord = "Bye Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ Assignments/Module-4/SpeakHello.js
@@ -0,0 +1,9 @@
+(function(window) {
+	var speakWord = "Hello & Welcome";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
